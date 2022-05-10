c8d3609bed57fae9559f77b153bf46854a90ae7d
Avent Chiu <65209258+sirAvent@users.noreply.github.com>
2022-05-09 20:47:46 -0700

    Update 7.js

--- problems/7.js
+++ problems/7.js
@@ -7,7 +7,8 @@
  * @example [1, 2, 3] -> 6
 */
 function problem(numbers) {
-    return null;
+    const sum = numbers.reduce(function (a, b) { return a + b; }, 0);
+    return sum;
 }
 
 const tests = [
